0853d63707f4bca6e1dd031e6c4b2358fe53a6be
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-08-28 04:30:22 +0530

    chore: create pm2 init file and add configs

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [
+    {
+      name   : "order-dispatch-service",
+      script : "java -jar ./order-dispatch-service/target/order-dispatch-service-1.0.0.jar",
+      log_file: "./logs/order-dispatch-service.log",
+      instances: 2
+    },
+    {
+      name   : "trip-telemetry-service",
+      script : "java -jar ./trip-telemetry-service/target/trip-telemetry-service-1.0.0.jar",
+      log_file: "./logs/trip-telemetry-service.log",
+      instances: 2
+    },
+    {
+      name   : "notification-service",
+      script : "java -jar ./notification-service/target/notification-service-1.0.0.jar",
+      log_file: "./logs/notification-service.log",
+      instances: 2
+    }
+  ]
+}
