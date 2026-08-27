3420f47adbef2f27c38c3a399f8cf53eee4ff306
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-08-28 05:03:56 +0530

    chore: add cloud SQL auth proxy configuration to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps : [
     {
+      name   : "cloud-sql-auth-proxy",
+      script : "./cloud-sql-proxy fleet-ops-506803:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy.log"
+    },
+    {
       name   : "order-dispatch-service",
       script : "java -jar ./order-dispatch-service/target/order-dispatch-service-1.0.0.jar",
       log_file: "./logs/order-dispatch-service.log",
